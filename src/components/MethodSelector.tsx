500aafb140352d583a5155d804d1acfa16f615dc
DialecticalLaw <d.shmuratkin@mail.ru>
2024-08-26 13:34:19 +0300

    feat: add color to methods

--- src/components/MethodSelector.tsx
+++ src/components/MethodSelector.tsx
@@ -1,7 +1,17 @@
 import { MenuItem, Select } from '@mui/material';
 import { useParams, useRouter } from 'next/navigation';
 
-const methods = ['get', 'post', 'put', 'delete', 'head', 'options'];
+const methods = [
+    { method: 'get', color: '#007f31' },
+    { method: 'post', color: '#ad7a03' },
+    { method: 'put', color: '#0053b8' },
+    { method: 'patch', color: '#623497' },
+    { method: 'delete', color: '#8e1a10' },
+    { method: 'head', color: '#007f31' },
+    { method: 'options', color: '#a61468' },
+];
+
+const findColor = (selectedMethod: string) => methods.find(({ method }) => selectedMethod === method).color;
 
 export default function MethodSelector() {
     const { replace } = useRouter();
@@ -9,12 +19,17 @@ export default function MethodSelector() {
 
     return (
         <Select
-            sx={{ borderTopRightRadius: 0, borderBottomRightRadius: 0 }}
+            sx={{
+                borderTopRightRadius: 0,
+                borderBottomRightRadius: 0,
+                color: findColor(request[0]),
+                fontWeight: 'bold',
+            }}
             value={request[0]}
             onChange={(e) => replace(`/${e.target.value}`)}
         >
-            {methods.map((method) => (
-                <MenuItem key={method} value={method}>
+            {methods.map(({ color, method }) => (
+                <MenuItem sx={{ color, fontWeight: 'bold' }} key={method} value={method}>
                     {method.toUpperCase()}
                 </MenuItem>
             ))}
