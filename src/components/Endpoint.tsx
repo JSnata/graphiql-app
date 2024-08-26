3b83420ff201f3493d9dca243bc2b2a95deadc95
DialecticalLaw <d.shmuratkin@mail.ru>
2024-08-26 21:45:24 +0300

    feat: reflect the endpoint in the url

--- src/components/Endpoint.tsx
+++ src/components/Endpoint.tsx
@@ -1,10 +1,15 @@
 import { TextField } from '@mui/material';
+import { useParams } from 'next/navigation';
 
 export default function Endpoint() {
+    const { request } = useParams<{ request: string[] }>();
+
     return (
         <TextField
+            onChange={(e) => window.history.replaceState(null, '', `/${request[0]}/${btoa(e.target.value)}`)}
             sx={{ width: '70%', '& .MuiInputBase-root': { borderTopLeftRadius: 0, borderBottomLeftRadius: 0 } }}
             label="Endpoint URL"
+            defaultValue={request[1] ? atob(decodeURIComponent(request[1])) : ''}
         />
     );
 }
