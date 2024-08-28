1fc234731ea9caf313632c4f31b7748a99ce6d3e
Linderjk <linder25@yandex.ru>
2024-08-28 16:17:05 +0300

    refactor: change styles for wrapper , add CssBaseline for reset default styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import Header from '@/components/shared/Header';
 import Footer from '@/components/shared/Footer';
 import { getServerSession } from 'next-auth';
 import SessionProvider from '@/providers/SessionProvider';
-import { Box, CssBaseline } from '@mui/material';
+import { Container, CssBaseline } from '@mui/material';
 import { NextIntlClientProvider } from 'next-intl';
 import { getLocale, getMessages } from 'next-intl/server';
 import React from 'react';
@@ -36,27 +36,37 @@ export default async function RootLayout({
     const messages = await getMessages();
     const session = await getServerSession(authOptions);
     return (
-        <html lang={locale}>
-            <body className={inter.className}>
-                <SessionProvider session={session}>
-                    <NextIntlClientProvider messages={messages}>
-                        <AppRouterCacheProvider>
-                            <StoreProvider>
-                                <ToastProvider>
-                                    <ThemeProvider theme={theme}>
-                                        <CssBaseline />
-                                        <Header />
-                                        <Box component="main" sx={{ minHeight: 'calc(100vh - 132px)' }}>
-                                            {children}
-                                        </Box>
-                                        <Footer />
-                                    </ThemeProvider>
-                                </ToastProvider>
-                            </StoreProvider>
-                        </AppRouterCacheProvider>
-                    </NextIntlClientProvider>
-                </SessionProvider>
-            </body>
-        </html>
+        <>
+            <CssBaseline />
+            <html lang={locale}>
+                <body className={inter.className}>
+                    <SessionProvider session={session}>
+                        <NextIntlClientProvider messages={messages}>
+                            <AppRouterCacheProvider>
+                                <StoreProvider>
+                                    <ToastProvider>
+                                        <ThemeProvider theme={theme}>
+                                            <Header />
+                                            <Container
+                                                component="main"
+                                                sx={{
+                                                    minHeight: 'calc(100vh - 132px)',
+                                                    display: 'flex',
+                                                    height: '100%',
+                                                    flexDirection: 'column',
+                                                }}
+                                            >
+                                                {children}
+                                            </Container>
+                                            <Footer />
+                                        </ThemeProvider>
+                                    </ToastProvider>
+                                </StoreProvider>
+                            </AppRouterCacheProvider>
+                        </NextIntlClientProvider>
+                    </SessionProvider>
+                </body>
+            </html>
+        </>
     );
 }
