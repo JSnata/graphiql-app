a5437145c7d59759d1d92ba29b4139b1c6d8401a
DialecticalLaw <d.shmuratkin@mail.ru>
2024-08-28 22:05:48 +0300

    feat: implement prettifying

--- src/components/HttpBody.tsx
+++ src/components/HttpBody.tsx
@@ -1,15 +1,31 @@
 'use client';
 
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import CodeMirror from '@uiw/react-codemirror';
 import { usePathname, useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { format } from 'prettier';
+import * as parser from 'prettier/plugins/babel';
+import * as estree from 'prettier/plugins/estree';
 
 export default function HttpBody() {
     const { replace } = useRouter();
     const pathname = usePathname();
     const params = pathname.split('/').filter(Boolean); // аналог useParams, обновляющийся при использовании history API
     const [code, setCode] = useState(atob(decodeURIComponent(params[2] || '')));
+    const [error, setError] = useState('');
+
+    const makeBeautify = async () => {
+        if (!code) return;
+        try {
+            if (code.trim().startsWith('{') && code.trim().endsWith('}')) {
+                setCode(await format(code, { parser: 'json', plugins: [parser, estree] }));
+            }
+            setError('');
+        } catch (err: unknown) {
+            if (err instanceof Error) setError(err.message);
+        }
+    };
 
     return (
         <Box sx={{ width: '100%' }}>
@@ -17,14 +33,21 @@ export default function HttpBody() {
                 value={code}
                 onChange={setCode}
                 onBlur={() => {
-                    if (params[1]) replace(`/${params[0]}/${params[1]}/${btoa(code)}`);
+                    if (params[1]) {
+                        replace(`/${params[0]}/${params[1]}/${btoa(code)}`);
+                    } else {
+                        setError('Please enter the endpoint');
+                    }
                 }}
-                placeholder="Enter the request body here"
+                placeholder="String or JSON"
                 theme="light"
                 height="150px"
                 style={{ fontSize: '18px' }}
             />
-            {!params[1] && <Typography sx={{ color: 'red' }}>Please enter the endpoint</Typography>}
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: '25px' }}>
+                <Button onClick={makeBeautify}>Beautify</Button>
+                {error && <Typography sx={{ color: 'red' }}>{error}</Typography>}
+            </Box>
         </Box>
     );
 }
