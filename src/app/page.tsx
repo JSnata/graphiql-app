d8a0df5cb69c4f73c1755cae6ae6e879b5b9450c
Linderjk <linder25@yandex.ru>
2024-08-28 16:18:40 +0300

    refactor: change styles feat: add translation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,65 @@
 'use client';
 
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Divider, Stack, Typography } from '@mui/material';
 import Link from 'next/link';
 import { signOut, useSession } from 'next-auth/react';
+import { useTranslations } from 'next-intl';
 
 export default function Home() {
     const { data: session, status } = useSession();
-
+    const t = useTranslations('Auth');
     if (status === 'loading') {
         return <div>Loading...</div>;
     }
 
     return (
-        <Box sx={{ margin: '0 auto', textAlign: 'center' }}>
-            {/* <Typography variant="h1">GraphQL Task</Typography> */}
-            <Link href="/restful">
-                <Button variant="contained">Restful</Button>
-            </Link>
+        <Box
+            sx={{
+                textAlign: 'center',
+                display: 'flex',
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
             <Box>
                 {session && (
-                    <div>
-                        <Typography variant="h5">Email: {session.user?.email}!</Typography>
-                        <Typography variant="h1">GraphQL Task</Typography>
+                    <Stack
+                        spacing={2}
+                        direction={{ xs: 'column', sm: 'row' }}
+                        divider={<Divider orientation="vertical" flexItem />}
+                    >
                         <Link href="/restful">
                             <Button variant="contained">Restful</Button>
                         </Link>
-                        <Button onClick={() => signOut()}>Sign Out</Button>
-                    </div>
+                        <Link href="/graphql">
+                            <Button variant="contained">GraphQL</Button>
+                        </Link>
+                        <Button variant="contained" color="secondary" onClick={() => signOut()}>
+                            {t('logout')}
+                        </Button>
+                    </Stack>
                 )}
                 {!session && (
                     <Box
                         sx={{
                             display: 'flex',
+                            flexDirection: 'column',
                             justifyContent: 'center',
                             gap: '10px',
                             alignItems: 'center',
-                            height: '100vh',
                         }}
                     >
-                        <Link href="/signin">
-                            <Button variant="contained">SignIn</Button>
-                        </Link>
-                        <Link href="/signup">
-                            <Button variant="contained">SignUp</Button>
-                        </Link>
+                        {' '}
+                        <Typography variant="h4">{`${t('welcome')} Restful/GraphQL Client`}</Typography>
+                        <Stack direction="row" spacing={2}>
+                            <Link href="/signin">
+                                <Button variant="contained">{t('signin')}</Button>
+                            </Link>
+                            <Link href="/signup">
+                                <Button variant="contained">{t('signup')}</Button>
+                            </Link>
+                        </Stack>
                     </Box>
                 )}
             </Box>
