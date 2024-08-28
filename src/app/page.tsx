e6d52094459535d0d15bd711da7aff30abe3acac
Linderjk <linder25@yandex.ru>
2024-08-28 10:58:45 +0300

    fix: add 3010 port for dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import Link from 'next/link';
 
 export default function Home() {
     return (
         <Box sx={{ margin: '0 auto', textAlign: 'center' }}>
-            <Typography variant="h1">GraphQL Task</Typography>
+            {/* <Typography variant="h1">GraphQL Task</Typography> */}
             <Link href="/restful">
                 <Button variant="contained">Restful</Button>
             </Link>
