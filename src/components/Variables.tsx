53b713814569c67d8880cfb8de30f31e5b6aa888
LinderJK <slepchenkovia@gmail.com>
2024-08-30 21:51:09 +0300

    refactor: change variable container and add methods

--- src/components/Variables.tsx
+++ src/components/Variables.tsx
@@ -1,64 +1,51 @@
 'use client';
 
-import { Box, Button, Paper, Stack, Typography } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { Box, Paper, Stack, Typography } from '@mui/material';
 import { useAppDispatch, useAppSelector } from '@/lib/hook';
-import { addVariable, clearVariables, removeVariable } from '@/lib/features/variablesSlice';
+import { addVariableField } from '@/lib/features/variablesSlice';
 import VariableInput from '@/components/ui/VariableInput';
-import { toast } from 'react-toastify';
 
 export default function Variables() {
     const variables = useAppSelector((state) => state.variables.variables);
-    const dispach = useAppDispatch();
+    const dispatch = useAppDispatch();
 
     const isEmpty = variables.find((data) => data.key === '' && data.value === '');
-    const handleAddVariable = () => {
-        if (!isEmpty) {
-            dispach(addVariable({ key: '', value: '' }));
-        } else {
-            toast.warn('Please set previous variable key and value first');
+    const handleAddVariable = (e: React.MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const targetElement = e.target as HTMLElement;
+        if (
+            targetElement.closest('button[aria-label="variable-delete"]') ||
+            targetElement.closest('.MuiIconButton-root')
+        ) {
+            return;
         }
-    };
 
-    const handleDelete = (key: string) => {
-        dispach(removeVariable(key));
+        if (!isEmpty) {
+            dispatch(addVariableField({ key: '', value: '' }));
+        }
     };
 
     return (
         <Box sx={{ alignItems: 'center', display: 'flex', flexDirection: 'column', width: '100%' }}>
             <Typography variant="h6">Varibales</Typography>
-            <Stack direction="row" spacing={2}>
-                <Button variant="contained" color="success" endIcon={<AddCircleIcon />} onClick={handleAddVariable}>
-                    Add
-                </Button>
-                <Button
-                    variant="contained"
-                    color="error"
-                    endIcon={<DeleteIcon />}
-                    onClick={() => dispach(clearVariables())}
+            <Box onClick={(e) => handleAddVariable(e)} sx={{ width: '100%', my: 2 }}>
+                <Paper
+                    sx={{
+                        height: '200px',
+                        overflow: 'auto',
+                        width: '100%',
+                        p: 2,
+                    }}
+                    elevation={1}
                 >
-                    Clear
-                </Button>
-            </Stack>
-
-            <Paper
-                sx={{
-                    height: '200px',
-                    overflow: 'auto',
-                    width: '100%',
-                    paper: 5,
-                    my: 2,
-                    p: 2,
-                }}
-                elevation={1}
-            >
-                <Stack direction="column" spacing={2}>
-                    {variables.map((data) => (
-                        <VariableInput key={data.key} variable={data} handleDelete={handleDelete} />
-                    ))}
-                </Stack>
-            </Paper>
+                    <Stack direction="column" spacing={2}>
+                        {variables.map((data, index) => (
+                            <VariableInput key={data.key} variable={data} index={index} />
+                        ))}
+                    </Stack>
+                </Paper>
+            </Box>
         </Box>
     );
 }
