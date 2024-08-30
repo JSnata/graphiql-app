137deca8ed40b2234004a45f3d570cb0fbf34df1
LinderJK <slepchenkovia@gmail.com>
2024-08-30 21:51:37 +0300

    feat: add crud operations for variables

--- src/components/ui/VariableInput.tsx
+++ src/components/ui/VariableInput.tsx
@@ -2,36 +2,85 @@
 
 import { IconButton, Stack, TextField } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { useAppDispatch, useAppSelector } from '@/lib/hook';
+import { removeVariableField, saveVariable } from '@/lib/features/variablesSlice';
+import { useState } from 'react';
+import SaveIcon from '@mui/icons-material/Save';
+import EditIcon from '@mui/icons-material/Edit';
+import { toast } from 'react-toastify';
 
 interface IVariableInputProps {
     variable: { [key: string]: string };
-    handleDelete: (key: string) => void;
+    index: number;
 }
 
 export default function VariableInput(props: IVariableInputProps) {
-    const { variable, handleDelete } = props;
-    // const [key, value] = useState(Object.entries(variable));
-    // console.log(variable, key, value);
+    const { variable, index } = props;
+    const allVariables = useAppSelector((state) => state.variables.variables);
+    const dispatch = useAppDispatch();
+    const [keyField, setKeyField] = useState(variable.key);
+    const [valueField, setValueField] = useState(variable.value);
+    const [editMode, setEditMode] = useState(false);
+
+    const handleKeyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setKeyField(e.target.value);
+    };
+
+    const handleValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValueField(e.target.value);
+    };
+
+    const handleSave = () => {
+        if (keyField === '' && valueField === '') return;
+        const isExist = allVariables.find((data) => data.key === keyField);
+        if (isExist) {
+            toast.error('Key already exists');
+            return;
+        }
+        dispatch(saveVariable({ key: keyField, value: valueField, selectedIndex: index }));
+        setEditMode(false);
+    };
+
+    const handleDelete = (keyValue: string) => {
+        dispatch(removeVariableField(keyValue));
+    };
+
     return (
-        <Stack
-            direction="row"
-            spacing={1}
-            // sx={{
-            //     alignItems: 'center',
-            //     display: 'flex',
-            //     justifyContent: 'space-between',
-            //     width: '100%',
-            // }}
-        >
+        <Stack direction="row" spacing={1} component="form">
             <TextField
                 size="small"
+                disabled={!editMode}
                 fullWidth
                 id="variable-key"
                 label="Key"
-                // onChange={(e) => (key[0] = e.target.value)}
+                value={keyField}
+                onChange={handleKeyChange}
             />
-            <TextField size="small" fullWidth id="variable-value" label="Value" />
-            <IconButton aria-label="variable-delete" size="small" onClick={() => handleDelete(variable.key)}>
+            <TextField
+                size="small"
+                fullWidth
+                disabled={!editMode}
+                id="variable-value"
+                label="Value"
+                value={valueField}
+                onChange={handleValueChange}
+            />
+
+            {editMode ? (
+                <IconButton aria-label="variable-delete" size="small" onClick={handleSave} color="success">
+                    <SaveIcon fontSize="inherit" />
+                </IconButton>
+            ) : (
+                <IconButton aria-label="variable-change" size="small" onClick={() => setEditMode(true)} color="primary">
+                    <EditIcon fontSize="inherit" />
+                </IconButton>
+            )}
+            <IconButton
+                aria-label="variable-delete"
+                size="small"
+                onClick={() => handleDelete(variable.key)}
+                color="error"
+            >
                 <DeleteIcon fontSize="inherit" />
             </IconButton>
         </Stack>
