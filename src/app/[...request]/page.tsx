8f80b1d0492881e7f1d55e5e8bdc7920a6ddd40e
LinderJK <slepchenkovia@gmail.com>
2024-08-31 14:41:05 +0300

    refactor: change styles

--- src/app/[...request]/page.tsx
+++ src/app/[...request]/page.tsx
@@ -1,28 +1,24 @@
-import { Box, Stack } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import Endpoint from '@/components/Endpoint';
-import HttpHeaders from '@/components/HttpHeaders';
 import MethodSelector from '@/components/MethodSelector';
 import HttpBody from '@/components/HttpBody';
 import HttpResponse from '@/components/HttpResponse';
 import TabsSection from '@/components/TabsSection';
-import Variables from '@/components/Variables';
+import HttpBodyVars from '@/components/HttpBodyVars';
+import HttpHeaders from '@/components/HttpHeaders';
 
 export default function Restful() {
     return (
-        <Stack
-            spacing={3}
-            component="section"
-            alignItems="center"
-            sx={{ margin: '0 auto', padding: '20px', width: '100%' }}
-        >
+        <Stack spacing={3} component="section" alignItems="center" sx={{ m: 2, width: '100%' }}>
+            <Typography variant="h4">Restful Client</Typography>
             <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
                 <MethodSelector />
                 <Endpoint />
             </Box>
 
             <TabsSection
-                labels={['Headers', 'Variables']}
-                elems={[<HttpHeaders key="headers" />, <Variables key="variables" />]}
+                labels={['Headers', 'Variables Body']}
+                elems={[<HttpHeaders key="headersVars" />, <HttpBodyVars key="bodyVars" />]}
             />
             <HttpBody />
             <HttpResponse />
