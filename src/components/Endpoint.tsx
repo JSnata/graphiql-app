f2975c103c680416d322e5a5fb70d4065d4ec64c
LinderJK <slepchenkovia@gmail.com>
2024-09-01 14:07:59 +0300

    refactor: add translation

--- src/components/Endpoint.tsx
+++ src/components/Endpoint.tsx
@@ -2,10 +2,11 @@
 
 import { TextField } from '@mui/material';
 import { useParams } from 'next/navigation';
+import { useTranslations } from 'next-intl';
 
 export default function Endpoint() {
     const { request } = useParams<{ request: string[] }>();
-
+    const t = useTranslations('Request');
     return (
         <TextField
             onChange={(e) =>
@@ -16,7 +17,7 @@ export default function Endpoint() {
                 )
             }
             sx={{ width: '70%', '& .MuiInputBase-root': { borderTopLeftRadius: 0, borderBottomLeftRadius: 0 } }}
-            label="Endpoint URL"
+            label={t('emptyEndpoint')}
             defaultValue={request[1] ? atob(decodeURIComponent(request[1])) : ''}
         />
     );
