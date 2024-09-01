6742520b588bdabb7939448febac02937e0a6811
LinderJK <slepchenkovia@gmail.com>
2024-09-01 14:08:38 +0300

    feat: add status code chip component

--- src/components/HttpResponse.tsx
+++ src/components/HttpResponse.tsx
@@ -1,17 +1,27 @@
 'use client';
 
-import { Box, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import CodeMirror from '@uiw/react-codemirror';
 import { useTranslations } from 'next-intl';
+import { useAppSelector } from '@/lib/hook';
+import ChipStatusCode from '@/components/ui/ChipStatusCode';
 
 export default function HttpResponse() {
     const t = useTranslations('Request');
-
+    const response = useAppSelector((state) => state.rest.responseBody);
+    const statusCode = useAppSelector((state) => state.rest.statusCode);
+    const formattedResponse = JSON.stringify(response, null, 2);
     return (
         <Box sx={{ width: '100%' }}>
-            <Typography>{t('status')}</Typography>
-            {t('body')}
-            <CodeMirror editable={false} readOnly theme="light" maxHeight="200px" style={{ fontSize: '16px' }} />
+            <Typography variant="h4" sx={{ textAlign: 'center' }}>
+                {t('response')}
+            </Typography>
+            <Stack direction="row" spacing={2} alignItems="center" sx={{ my: 2 }}>
+                <Typography variant="body1">{t('status')}</Typography>
+                <ChipStatusCode statusCode={statusCode} />
+            </Stack>
+            <Typography variant="body1">{t('responseBody')}</Typography>
+            <CodeMirror editable={false} value={formattedResponse} readOnly theme="light" height="150px" />
         </Box>
     );
 }
