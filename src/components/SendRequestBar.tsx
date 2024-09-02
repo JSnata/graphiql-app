881f9df869ba0f9ccb8c98b7565473b539fcc62c
Linderjk <linder25@yandex.ru>
2024-09-02 14:23:55 +0300

    feat: change logic send reques fnc. add errors handling, add function generate headers and body

--- src/components/SendRequestBar.tsx
+++ src/components/SendRequestBar.tsx
@@ -4,41 +4,51 @@ import MethodSelector from '@/components/MethodSelector';
 import Endpoint from '@/components/Endpoint';
 import { Box, Button } from '@mui/material';
 import { usePathname } from 'next/navigation';
-import { useAppDispatch } from '@/lib/hook';
+import { useAppDispatch, useAppSelector } from '@/lib/hook';
 import { setResponseBody, setStatusCode } from '@/lib/features/restSlice';
 import { useTranslations } from 'next-intl';
+import generateRequestBodyWithVars, { generateHeaders } from '@/utils/generateRequestBodyWithVars';
+import { toast } from 'react-toastify';
 
 export default function SendRequestBar() {
     const pathname = usePathname();
     const t = useTranslations('Request');
     const dispatch = useAppDispatch();
+    const variablesBody = useAppSelector((state) => state.variables.variablesBody);
+    const headers = useAppSelector((state) => state.variables.variablesHeader);
+
     const handleSend = async () => {
         const requestParams = pathname.split('/').filter(Boolean);
         const decodeData = {
-            method: requestParams[0],
-            url: requestParams[1] ? atob(requestParams[1]) : '',
-            body: requestParams[2] ? atob(requestParams[2]) : null,
+            method: requestParams[0].toUpperCase(),
+            url: requestParams[1] ? atob(decodeURIComponent(requestParams[1])) : '',
+            body: requestParams[2] ? atob(decodeURIComponent(requestParams[2])) : null,
         };
 
         if (decodeData.url) {
             const requestOptions: RequestInit = { method: decodeData.method };
-            if (decodeData.body) {
-                requestOptions.body = decodeData.body;
-                requestOptions.headers = {
-                    'Content-Type': 'application/json',
-                };
+            try {
+                if (decodeData.body && decodeData.method !== 'GET') {
+                    requestOptions.body = generateRequestBodyWithVars(decodeData.body, variablesBody);
+                    requestOptions.headers = generateHeaders(headers);
+                }
+            } catch (err) {
+                toast.warn(err.message);
             }
-            const response = await fetch(
-                decode,
-                { method: requestParams[0] },
-                // { headers: { 'Content-Type': 'application/json' } },
-                // { body: decode },
-            );
-            const data = await response.json();
+
+            try {
+                const response = await fetch(decodeData.url, requestOptions);
+                const data = await response.json();
+                dispatch(setStatusCode(response.status));
+                dispatch(setResponseBody(data));
+            } catch (err) {
+                console.error(err);
+                toast.error(`Error ${err}`);
+            }
+            // console.log(requestOptions);
+
             // console.log(data);
             // console.log(response);
-            dispatch(setStatusCode(response.status));
-            dispatch(setResponseBody(data));
         }
     };
     return (
