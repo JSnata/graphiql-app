c2491bfa6b87182c309c0c9e8b04d0d694716ea5
Linderjk <linder25@yandex.ru>
2024-09-02 14:28:14 +0300

    feat: add methods to slice

--- src/lib/features/restSlice.ts
+++ src/lib/features/restSlice.ts
@@ -2,12 +2,14 @@ import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 
 export interface RestSlice {
     method: string;
+    requestBody: { [key: string]: string };
     responseBody: { [key: string]: string };
     statusCode: number;
 }
 
 const initialState: RestSlice = {
-    method: '',
+    method: 'get',
+    requestBody: {},
     responseBody: {},
     statusCode: null,
 };
@@ -22,8 +24,14 @@ export const restSlice = createSlice({
         setStatusCode(state, action: PayloadAction<number>) {
             state.statusCode = action.payload;
         },
+        setMethod(state, action: PayloadAction<string>) {
+            state.method = action.payload;
+        },
+        setRequestBody(state, action: PayloadAction<{ [key: string]: string }>) {
+            state.requestBody = action.payload;
+        },
     },
 });
 
-export const { setResponseBody, setStatusCode } = restSlice.actions;
+export const { setResponseBody, setStatusCode, setMethod } = restSlice.actions;
 export default restSlice.reducer;
