90f14be48776ce58af79a42d8dac6f2d54f5c9df
LinderJK <slepchenkovia@gmail.com>
2024-09-07 15:56:09 +0300

    feat: add change headers to base64 and set in url

--- src/components/HttpHeaders.tsx
+++ src/components/HttpHeaders.tsx
@@ -4,8 +4,12 @@
 import { useAppDispatch, useAppSelector } from '@/lib/hook';
 import { addVariableHeaderField, removeVariableHeaderField, saveHeaderVariable } from '@/lib/features/variablesSlice';
 import VariablesField from '@/components/ui/VariablesField';
+import { usePathname, useRouter } from 'next/navigation';
+import { useCallback } from 'react';
 
 export default function HttpHeaders() {
+    const { replace } = useRouter();
+    const pathname = usePathname();
     // return (
     //     <Box
     //         sx={{
@@ -31,17 +35,35 @@ export default function HttpHeaders() {
     //         </Box>
     //     </Box>
     // );
+
     const variablesHeader = useAppSelector((state) => state.variables.variablesHeader);
     const dispatch = useAppDispatch();
+
+    const encodeHeadersToBase64 = useCallback((headers: { [key: string]: string }[]) => {
+        return headers
+            .filter((header) => header.key && header.value)
+            .map((header) => `${encodeURIComponent(header.key)}=${encodeURIComponent(header.value)}`)
+            .join('&');
+    }, []);
+
+    const updateUrlWithHeaders = useCallback(() => {
+        const encodedHeaders = encodeHeadersToBase64(variablesHeader);
+        const updatedUrl = `${pathname}?${encodedHeaders}`;
+        replace(updatedUrl);
+    }, [encodeHeadersToBase64, variablesHeader, pathname, replace]);
     const handleSave = (keyField: string, valueField: string, index: number) => {
         dispatch(saveHeaderVariable({ key: keyField, value: valueField, selectedIndex: index }));
+        updateUrlWithHeaders();
     };
     const handleRemove = (keyValue: string) => {
         dispatch(removeVariableHeaderField(keyValue));
+        updateUrlWithHeaders();
     };
     const handleAdd = ({ key, value }: { key: string; value: string }) => {
         dispatch(addVariableHeaderField({ key, value }));
+        updateUrlWithHeaders();
     };
+
     return (
         <VariablesField
             variables={variablesHeader}
