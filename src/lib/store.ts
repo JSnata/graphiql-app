315fa2ade6478ff2b1ece11be1a9e367185b191c
LinderJK <slepchenkovia@gmail.com>
2024-09-14 08:05:17 +0300

    feat: add graphql reducer

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import variablesReducer from './features/variablesSlice';
 import restReducer from './features/restSlice';
+import graphqlReducer from './features/graphqlSlice';
 
 export const makeStore = () => {
     return configureStore({
         reducer: {
             variables: variablesReducer,
             rest: restReducer,
+            graphql: graphqlReducer,
         },
         // middleware: (getDefaultMiddleware) => {
         //   return getDefaultMiddleware().concat(quotesApiSlice.middleware);
