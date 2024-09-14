6429cc3d41758acda7e07e24293ba92d4eaa4daf
LinderJK <slepchenkovia@gmail.com>
2024-09-14 08:00:51 +0300

    feat: add graphql vars

--- src/utils/generateRequestBodyWithVars.ts
+++ src/utils/generateRequestBodyWithVars.ts
@@ -1,6 +1,15 @@
-export default function generateRequestBodyWithVars(body: string, variables: { [key: string]: string }[]) {
-    const regex = /{{(.*?)}}/g;
+export default function generateRequestBodyWithVars(
+    body: string,
+    variables: {
+        [key: string]: string;
+    }[],
+    useBrackets = true,
+) {
+    const regexBrackets = /{{(.*?)}}/g;
+    const regexDollar = /\$(\w+)/g;
+    // console.log(variables, variables.length, 'variables');
     const replaceVariables = (str: string) => {
+        const regex = useBrackets ? regexBrackets : regexDollar;
         return str.replace(regex, (match, variableName) => {
             const variable = variables.find((v) => v.key === variableName);
             if (!variable) {
@@ -9,12 +18,17 @@ export default function generateRequestBodyWithVars(body: string, variables: { [
             return `"${variable.value}"`;
         });
     };
+    if (variables.length === 0) {
+        return body;
+    }
     return replaceVariables(body);
 }
 
 export function generateHeaders(headers: { [key: string]: string }[]) {
     const headersObject = new Headers();
-
+    if (headers.length === 0) {
+        return headersObject;
+    }
     headers.forEach((header) => {
         if (header.key && header.value && header.value.trim().length > 0) {
             headersObject.set(header.key, header.value);
