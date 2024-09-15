9e674928e3769c98b58ec4d13eac5d99b7ae17c1
DialecticalLaw <d.shmuratkin@mail.ru>
2024-09-16 01:32:01 +0300

    feat: improve design of the cards with info

--- src/components/ui/InfoCard.tsx
+++ src/components/ui/InfoCard.tsx
@@ -1,35 +1,48 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
-import { useTranslations } from 'next-intl';
+import { Box, Divider, Typography } from '@mui/material';
 import Link from 'next/link';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 export default function InfoCard({ name, github, location, githubLink }) {
-    const t = useTranslations('About');
     return (
         <Box
             sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 0.5,
                 maxWidth: 300,
                 p: 2,
-                border: '1px solid #ddd',
+                border: '1px solid #dddddd',
                 borderRadius: 2,
                 textAlign: 'left',
-                transition: 'transform 0.2s',
+                transition: '0.2s',
                 '&:hover': {
                     transform: 'scale(1.03)',
-                    borderColor: '#000',
+                    cursor: 'default',
+                    boxShadow: '0 5px 15px 0.5px #7c7c7c',
                 },
             }}
         >
-            <Typography component="div">{name}</Typography>
-            <Typography sx={{ mt: 1 }} variant="body2">
-                {`${t('github')}`}:{' '}
-                <Link href={githubLink} target="_blank" rel="noopener">
+            <Typography textAlign="center" component="div">
+                {name}
+            </Typography>
+            <Divider />
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 1 }}>
+                <GitHubIcon />
+                <Link
+                    style={{ fontWeight: 'bold', textDecoration: 'underline' }}
+                    href={githubLink}
+                    target="_blank"
+                    rel="noopener"
+                >
                     {github}
                 </Link>
-            </Typography>
-            <Typography variant="body2" sx={{ mt: 1 }}>
-                {`${t('location')}`}: {location}
-            </Typography>
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 1 }}>
+                <LocationOnIcon />
+                {location}
+            </Box>
         </Box>
     );
 }
